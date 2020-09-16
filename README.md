6a65fc675ba521957328347cd3a96c08d756d431
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-16 16:20:38 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Oxford Comma List Formatter
+yeaan if # Oxford Comma List Formatter
 
 ## Objectives
 
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.length == 2
+    return (array).join (" and ")
+  elsif array.length >= 3
+    (array[-1]).insert( index to insert into ,"and")
+  else 
+    array
+  end
+  (array)[-2].join(" , ")
 end
+
