1034be8b2c4e42a05557fc0dd4b35457ed8524de
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-16 16:47:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.length == 2
+    return array.join (" and ")
+  elsif array.length >= 3
+    array[-1].insert(0,"and ")
+  else 
+    array
+  end
+  array.join(", ")
 end
+
