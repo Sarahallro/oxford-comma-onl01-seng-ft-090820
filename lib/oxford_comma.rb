8d7fb314055db218485924be615454cc0dedec7c
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-16 16:32:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,12 @@
 
 def oxford_comma(array)
   if array.length == 2
-    return (array).join (" and ")
+    return array.join (" and ")
   elsif array.length >= 3
-    (array[-1]).insert(0,"and")
+    array[-1].insert(0,"and")
   else 
     array
   end
-  (array).join(" , ")
+  array.join(" , ")
 end
 
